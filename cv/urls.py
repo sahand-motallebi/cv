b543ea686f8efbe3491a8acdd5e10f9fd7d7d92b
sahand-motallebi <sahandmotallebi@outlook.com>
2024-09-22 02:34:07 +0330

    some change

--- cv/urls.py
+++ cv/urls.py
@@ -15,9 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="APP API",
+        default_version="v1",
+        description="Brief descriptions about the api",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="bigdeli.ali3@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('sahandmt/', admin.site.urls),
@@ -27,3 +48,48 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+if settings.COMINGSOON:
+    urlpatterns.insert(
+        0, re_path(r"^", TemplateView.as_view(template_name="comingsoon.html"))
+    )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+
+if settings.SHOW_DEBUGGER_TOOLBAR:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+
+if settings.SHOW_SWAGGER:
+    urlpatterns += [
+        path(
+            "api-auth/",
+            include("rest_framework.urls", namespace="rest_framework"),
+        ),
+        path(
+            f"api/swagger/api.json",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        path(
+            f"api/swagger/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+    ]
+
+
+
+handler400 = "cv.error_views.error_400"  # bad_request
+handler403 = "cv.error_views.error_403"  # permission_denied
+handler404 = "cv.error_views.error_404"  # page_not_found
+handler500 = "cv.error_views.error_500"  # server_error
